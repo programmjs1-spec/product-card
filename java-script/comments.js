97e3ee9fa2562f6702e749d4a9ce857bd27a0e40
abdulkabeer <programmjs1@gmail.com>
2026-03-27 22:08:28 +0300

    working with array methods

---
+++ java-script/comments.js
@@ -0,0 +1,74 @@
+// 1.Экспорт 10 объектов и добавление ключевого слова "export".
+
+export const socialMediaComments = [
+  {
+    postId: 1,
+    id: 1,
+    name: "id labore ex et quam laborum",
+    email: "Eliseo@gardner.biz",
+    body: "laudantium enim quasi est quidem magnam voluptate ipsam eos..."
+  },
+  {
+    postId: 1,
+    id: 2,
+    name: "quo vero reiciendis velit similique earum",
+    email: "Jayne_Kuhic@sydney.com",
+    body: "est natus enim nihil est dolore omnis voluptatem numquam..."
+  },
+  {
+    postId: 1,
+    id: 3,
+    name: "odio adipisci rerum aut animi",
+    email: "Nikolaus@gerhold.net",
+    body: "quia molestiae reprehenderit quasi aspernatur..."
+  },
+  {
+    postId: 1,
+    id: 4,
+    name: "alias odio sit",
+    email: "Lew@alysha.tv",
+    body: "non et atque occaecati deserunt quas accusantium..."
+  },
+  {
+    postId: 1,
+    id: 5,
+    name: "vero eaque aliquid doloribus et culpa",
+    email: "Hayden@althea.biz",
+    body: "harum non quasi et ratione tempore iure ex voluptates..."
+  },
+  {
+    postId: 2,
+    id: 6,
+    name: "et fugit eligendi deleniti quidem",
+    email: "Maxime@lola.org",
+    body: "doloribus at sed quis culpa deserunt consectetur..."
+  },
+  {
+    postId: 2,
+    id: 7,
+    name: "repellat consequatur praesentium vel minus molestias",
+    email: "Delpha@melany.io",
+    body: "maiores sed dolores similique labore et inventore..."
+  },
+  {
+    postId: 2,
+    id: 8,
+    name: "et omnis dolorem",
+    email: "Carroll@alexane.org",
+    body: "ut voluptatem corrupti velit ad voluptatem maiores..."
+  },
+  {
+    postId: 2,
+    id: 9,
+    name: "provident id voluptas",
+    email: "Rey.Padberg@karina.biz",
+    body: "sapiente assumenda molestiae atque..."
+  },
+  {
+    postId: 2,
+    id: 10,
+    name: "eaque et deleniti atque tenetur ut quo ut",
+    email: "Lemuel@jessyca.biz",
+    body: "voluptate iusto quis nobis reprehenderit ipsum amet nulla..."
+  }
+];
